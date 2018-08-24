b220a611afff6bdf654aa2d8e3542dfc51b02506
hauekaie <hauekaie@gmail.com>
2018-08-24 15:04:28 -0600

    Add Holaap message And a SOUT without comment

--- src/NS_Helloworld/Main.java
+++ src/NS_Helloworld/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Hello everyone");
         System.out.println("Holaap");
+        System.out.println();
     }
 }
